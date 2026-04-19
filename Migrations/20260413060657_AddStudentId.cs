52d97d1a342ec0a4eb1318b9fc19e20033216249
BinuuuSam <binudisamarasinghe@gmail.com>
2026-04-19 13:50:06 +0530

    Add files via upload

---
+++ Migrations/20260413060657_AddStudentId.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PAS_Full_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStudentId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "StudentId",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StudentId",
+                table: "AspNetUsers");
+        }
+    }
+}
