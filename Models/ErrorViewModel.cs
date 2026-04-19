209c6c0ae2b51b14958e16989c9d61e1e2b7ba67
nmedussuriya <nularamethnadi@gmail.com>
2026-04-20 00:07:14 +0530

    ErrorView

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PAS_Full_System.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
