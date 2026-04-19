ac3b6df354aae8627ca1af549e7bf4a74a54a0c8
BinuuuSam <binudisamarasinghe@gmail.com>
2026-04-19 21:22:04 +0530

    Add files via upload

---
+++ Migrations/20260413061445_AddProjectFields.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PAS_Full_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProjectFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GroupMemberIds",
+                table: "Projects",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "GroupMemberNames",
+                table: "Projects",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProposalFileName",
+                table: "Projects",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProposalFilePath",
+                table: "Projects",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GroupMemberIds",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "GroupMemberNames",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "ProposalFileName",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "ProposalFilePath",
+                table: "Projects");
+        }
+    }
+}
---
+++ Migrations/20260417150619_AddSupervisorExpertise.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PAS_Full_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSupervisorExpertise : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SupervisorExpertises",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SupervisorId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ResearchAreaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SupervisorExpertises", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SupervisorExpertises_AspNetUsers_SupervisorId",
+                        column: x => x.SupervisorId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SupervisorExpertises_ResearchAreas_ResearchAreaId",
+                        column: x => x.ResearchAreaId,
+                        principalTable: "ResearchAreas",
+                        principalColumn: "ResearchAreaId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SupervisorExpertises_ResearchAreaId",
+                table: "SupervisorExpertises",
+                column: "ResearchAreaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SupervisorExpertises_SupervisorId",
+                table: "SupervisorExpertises",
+                column: "SupervisorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SupervisorExpertises");
+        }
+    }
+}
